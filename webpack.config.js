eeb086151db528ff1116498e39c61b1fd29c355c
Juhani Ovalo <juhani.ovalo@gmail.com>
2024-09-04 09:09:53 +0300

    Different colors for html template

--- webpack.config.js
+++ webpack.config.js
@@ -54,8 +54,8 @@ module.exports = {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Webpack App with Canvas</title>
             <style>
-              html, body { background-color: #a0a0a0; display: flex; justify-content: center; }
-              #game { border: 1px solid darkgrey }
+              html, body { background-color: green; display: flex; justify-content: center; }
+              #game { background-color: black }
             </style>
         </head>
         <body>
