9d7e3727d7835e219a3c91fbc80cfe228e00b108
Juhani Ovalo <juhani.ovalo@gmail.com>
2024-09-11 08:52:34 +0300

    Ensure that table is created as empty

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,24 @@
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const table = [];
 
-export function getStage1Table(rows, cols) {
-  console.log("game table ", rows, cols);
+export function getStage1Table(rows, cols, level) {
+  let table = [];
   for (let i = 0; i < rows; i++) {
-    const row = [];
+    let row = [];
     for (let j = 0; j < cols; j++) {
-      const num1 = getRandomNumber(1, 15); // Satunnainen numero välillä 1-15
-      const blur = getRandomNumber(3, 7); // Satunnainen numero välillä 0-5
-      const clicked = false; // Boolean-arvo clicked
-      row.push({ num1, blur, clicked }); // Lisätään objekti riviin
+      const num1 = getRandomNumber(1, 15); // numbers to use in blocks
+      const blur = getRandomNumber(3 + level, 7 + level); // random blur, add level to make harder to guess number
+      const clicked = false; // Boolean clicked
+      row.push({ num1, blur, clicked });
+      console.log("row ", row);
     }
+    // Lets ensure we have enough number 13
+    for (let k = 0; k < cols / 3; k++) {
+      row[k]["num1"] = 13;
+    }
+    row = row.sort(() => Math.random() - 0.5);
+
     table.push(row); // Lisätään rivi taulukkoon
   }
   return table;
